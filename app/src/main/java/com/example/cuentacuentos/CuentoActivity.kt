1cfc24533821098636d068ddc342e952b30850e6
Marc <marc.montoya@mataro.epiaedu.cat>
2024-01-31 21:02:00 +0100

    Reproductor de video básico funcional

---
+++ app/src/main/java/com/example/cuentacuentos/CuentoActivity.kt
@@ -0,0 +1,48 @@
+package com.example.cuentacuentos
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cuentacuentos.R
+
+class CuentoActivity : AppCompatActivity() {
+    private lateinit var videoView: VideoView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cuento_view)
+
+        videoView = findViewById(R.id.videoView)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.evilgoblin)
+        videoView.setVideoURI(videoUri)
+
+        // Agregar OnClickListener al VideoView
+        videoView.setOnClickListener {
+            if (videoView.isPlaying) {
+                // Si está reproduciendo, pausar
+                videoView.pause()
+            } else {
+                // Si no está reproduciendo, iniciar la reproducción
+                videoView.start()
+            }
+        }
+
+        // Iniciar la reproducción del video
+        videoView.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Pausar el video cuando la actividad está en segundo plano
+        if (videoView.isPlaying) {
+            videoView.pause()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Detener la reproducción del video al salir de la actividad
+        videoView.stopPlayback()
+    }
+}
--- app/src/main/java/com/example/cuentacuentos/MainActivity.kt
+++ app/src/main/java/com/example/cuentacuentos/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.cuentacuentos
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //test >:3c
+        val btncuento: Button = findViewById(R.id.btncuento)
+        btncuento.setOnClickListener {
+            val intent = Intent(this, CuentoActivity::class.java)
+            startActivity(intent)
+        }
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
